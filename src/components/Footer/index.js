c120d2c9f5b6831771bb84f312e6157422b4f092
BriagasD98 <briagasdavid@yahoo.com>
2021-09-24 14:19:34 -0500

    sample text added into components for Nav testing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,10 @@ function Footer() {
         <footer>
             <div>
                 <a href="https://github.com/BriagasD98" target="_blank">
-                    {/* add github png here */}
+                    {/* add github png here */} Heres my github
                 </a>
                 <a href="https://www.linkedin.com/in/david-briagas-a55782210/" target="_blank">
-                    {/* add LinkedIn png here */}
+                    {/* add LinkedIn png here */} Heres my linkedin
                 </a>
             </div>
         </footer>
