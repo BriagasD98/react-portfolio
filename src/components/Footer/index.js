c9078041ee9ec538966abf62cc83353c19ecd309
BriagasD98 <briagasdavid@yahoo.com>
2021-09-27 01:00:07 -0500

    footer content added, contact icons added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,26 @@
 import React from "react";
+import gitIcon from '../../assets/images/Github-icon.png'
+// import emailIcon from '../../assets/images/email-icon.png'
+import linkedIn from '../../assets/images/linkedin-icon.png'
+import stackOicon from '../../assets/images/stackO-icon.png'
 
 function Footer() {
 
     return (
-        <footer>
-            <div className="footer fixed-bottom">
-                <a href="https://github.com/BriagasD98" className="mx-2" target="_blank">
-                    {/* add github png here */} Heres my github
+        <footer className="footer fixed-bottom">
+            <div>
+                <a href="https://github.com/BriagasD98" target="_blank" className="mx-2">
+                    <img src={gitIcon} alt="Github Link"/>
                 </a>
-                <a href="https://www.linkedin.com/in/david-briagas-a55782210/" className="mx-2" target="_blank">
-                    {/* add LinkedIn png here */} Heres my linkedin
+            </div>
+            <div>
+                <a href="https://stackoverflow.com/users/15757837/david-briagas" target="_blank" className="mx-2">
+                    <img src={stackOicon} alt="StackOverflow Link" className="emailIcon"/>
+                </a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/david-briagas-a55782210" target="_blank" className="mx-2">
+                    <img src={linkedIn} alt="LinkedIn Link" className="linkedinIcon"/>
                 </a>
             </div>
         </footer>
