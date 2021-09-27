fd43f7ea57524b7c9438c9aa14db52f21b983d0f
BriagasD98 <briagasdavid@yahoo.com>
2021-09-26 23:50:44 -0500

    About component content added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+// import DavidB from '../../assets/images/david-headshot.jpg'
 
 function About() {
 
     return (
-        <h1>Hello, Im About</h1>
+        <div>
+            <div>
+                {/* <img src={DavidB} alt="David Briagas Smiling" /> */}
+            </div>
+            <div>
+                <p> My name is David Briagas and I'm 23 years old. I was born and raised in Houston,Texas. My hobbies
+                are photography, auto mechanics and spending money on rusty 90's cars. I own two 90s Miatas - One
+                which i daily drive and the other a track car project. Most people know me as "the guy with the Miata".
+                </p>
+                <p> I graduated from Cinco Ranch High School in 2016. I studied Full-Stack Web Development at UT Austin
+                    from April 2021 to October 2021 and recieved a certificate of completion. I gained a great amount of 
+                    invaluable knowledge while attending the program and i'm currently looking for employment in the
+                    Web Development field. As I search for employment, I continue to practice and expand my knowledge 
+                    in Web Development so that I may be employer-competitive.
+                </p>
+            </div>
+        </div>
     )
 };
 
